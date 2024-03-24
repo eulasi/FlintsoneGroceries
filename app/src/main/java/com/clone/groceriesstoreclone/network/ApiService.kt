37f99c88c46bb672cbb442a0db7cd5b2aad67b4d
Emmanuel <96310496+eulasi@users.noreply.github.com>
2024-03-24 10:43:44 -0400

    build: API,APK,build files Updated

--- app/src/main/java/com/clone/groceriesstoreclone/network/ApiService.kt
+++ app/src/main/java/com/clone/groceriesstoreclone/network/ApiService.kt
@@ -5,9 +5,9 @@ import com.clone.groceriesstoreclone.model.Product
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("https://run.mocky.io/v3/5e9207a6-7dee-49a8-bcda-6933608e0335")
+    @GET("5e9207a6-7dee-49a8-bcda-6933608e0335")
     suspend fun getProducts(): List<Product>
 
-    @GET("https://run.mocky.io/v3/e36d1085-0abd-4a34-9e6b-dcc64e120641")
+    @GET("e36d1085-0abd-4a34-9e6b-dcc64e120641")
     suspend fun getCategories(): List<Category>
 }
